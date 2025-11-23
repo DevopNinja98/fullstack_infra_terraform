dd9bfc404f1a6325960e7ab8a4203da8920497cc
DevopNinja98 <divindignity@proton.me>
2025-11-23 18:22:26 +0530

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "niranjan-test-bucket"
+    bucket = "niranjan-test1-bucket1"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
